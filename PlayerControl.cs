c6e571b9a1f3b6feeea31ce65c5d1ddcc3370508
delgerzaya95 <delgerzaya95@gmail.com>
2018-05-30 19:49:33 +0900

    Add files via upload

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -1,43 +1,41 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour {
-
-	public float speed = 7f;
-	public bool isSprinting = false;
+	private CharacterController controller;
+	private float verticalVelocity;
+	private float gravity;
+	private float jumpForce;
+	public float speed;
+	public float health;
+	public	bool moveAnim;
+	public DeathMenu theDeathScreen;
+	float h;
+	float v;
 	Vector3 movement;
-//	Animator anim;
-	Rigidbody playerRigidbody;
-	int floorMask;
-	float camRayLength = 100f;
-	public int health = 100;
-	public bool isHealing;
-	int healAmount = 0;
-	bool walking;
-	float regenerationSpeed = 1.0f;
-	float defaultY;
-	/*
-	 * energy - stamina | regenSpeed | melee? dmg
-	 * agility - movespeed | critmodifier
-	 * endurance - sickness resist|duration | total health
-	 * perception - ranged dmg
-	 */
-
-
-
-	void Awake()
-	{
-		floorMask = LayerMask.GetMask ("Floor");
-//		anim = GetComponent<Animator> ();
-		playerRigidbody = GetComponent<Rigidbody> ();
-		isHealing = false;
-		defaultY = 1.5f;
+	Vector3 camLookDir;
+	BloodSc bs;
+	Animator pAnim;
+	// Use this for initialization
+	void Start () {
+		moveAnim = false;
+		gravity = 14.0f;
+		jumpForce = 10.0f;
+		speed  = 10.0f;
+		health = 100.0f;
+	//	Cursor.lockState = CursorLockMode.Locked;	
+		bs = GameObject.FindGameObjectWithTag("Canvas").GetComponentInChildren<BloodSc>();
+		controller = gameObject.GetComponent<CharacterController> ();
+		pAnim = gameObject.GetComponent<Animator> ();
+	//	sword = gameObject.
 	}
-
-	void FixedUpdate()
-	{
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
+	// Update is called once per frame
+	void Update () {
+		Event e = Event.current;
+	//	h = Input.GetAxisRaw ("Horizontal");
+	//	v = Input.GetAxisRaw ("Vertical");
 		if (Input.GetKey (KeyCode.LeftShift)) {
 			if (speed >= 20f) {
 				speed  = 20f;
@@ -52,77 +50,85 @@ public class PlayerControl : MonoBehaviour {
 				speed -= 0.2f;
 			}
 		}
-		Move (h, v, speed);
-		//function calls
-		Turning ();
-//		Animating (h, v); 
-		if (isHealing && Time.timeScale != 0) {
-			StartCoroutine(Regenerate(isHealing, healAmount, regenerationSpeed));
-		}
-		transform.position = new Vector3 (transform.position.x, defaultY, transform.position.z);
-	}
 
-	void Move(float h, float v, float runSpeed)
-	{
-		movement.Set (h, 0f, v);
-		movement = movement.normalized * runSpeed * Time.deltaTime;
-		playerRigidbody.MovePosition (transform.position + movement);
-	}
-
-	void Turning()
-	{
-		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit floorHit;
-		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
-			Vector3 playerToMouse = floorHit.point - transform.position; //vector from player to mouse location
-			//TODO: transform.position <- position of the player
-			playerToMouse.y = 0f; //not to make player lean back
-			Quaternion newRotation = Quaternion.LookRotation(playerToMouse); // Quaternion for rotation, not Vector3
-			playerRigidbody.MoveRotation(newRotation);
-		}
-	}
-
-	public void Heal(bool regenOn, int hp)
-	{
-		if (isHealing) {
-			healAmount += hp;
-		}
-		isHealing = regenOn;
-		healAmount = hp;
-	}
-	IEnumerator Regenerate(bool regenOn, int a, float regenSpeed)
-	{
-		isHealing = false;
-		while( a > 0) {
-			health = health + (int)regenSpeed; //(int)(regenSpeed * Time.deltaTime)
-			yield return null;
-			a--;
-		}
-	//	Debug.Log ("Regen complete");
-		yield return null;
-		CancelInvoke ();
-	}
-
-	void Animating(float h, float v)
-	{
-//		walking = (h != 0f || v != 0f);
-//		anim.SetBool ("IsWalking", walking); //name of the animation in Animation Controller
-	}
-
-	public void TakeDamage(int dmg) //Vector3 Position
-	{
-		health -= dmg;
 		if (health <= 0) {
-			Die ();
+			Invoke ("Die", 1);
 		}
+
+		if (Input.GetKey (KeyCode.Q)) {
+			//	transform.RotateAround (transform.position, new Vector3 (0, 1, 0), 1);
+			transform.Rotate (new Vector3 (0, -1, 0));//, Space.World);
+		} else if (Input.GetKey (KeyCode.E)) {
+			//	transform.RotateAround (transform.position, new Vector3 (0, 1, 0), -1);
+			transform.Rotate (new Vector3 (0, 1, 0));//, Space.World);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward.normalized * speed * 0.01f;
+			moveAnim = true;
+			pAnim.SetBool ("moving", moveAnim);
+		} else if (Input.GetKey (KeyCode.S)) {
+			moveAnim = true;
+			pAnim.SetBool ("moving", moveAnim);
+			transform.position += transform.forward.normalized * (-1) * speed * 0.01f;
+		} else {
+			moveAnim = false;
+			pAnim.SetBool ("moving", moveAnim);
+		}
+	//	if (Input.GetKeyDown (KeyCode.Space)) {
+	//		gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 300, 0));
+	//	}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += transform.right.normalized * (-1) * speed * 0.01f;
+		} else if (Input.GetKey (KeyCode.D)) {
+			transform.position += transform.right.normalized * speed * 0.01f;
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+			bs.TakeDamage ();
+			health -= 100f;
+		}
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Attack ();
+		}
+		if (controller.isGrounded) {
+			verticalVelocity = -gravity * Time.deltaTime;
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				verticalVelocity = jumpForce;
+			}
+		} else {
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
+		Vector3 moveVector = Vector3.zero;
+		moveVector.x = 0.0f;
+		moveVector.y = verticalVelocity;
+		moveVector.z = 0.0f;
+		controller.Move (moveVector * Time.deltaTime);
 	}
 
-	void Die()
-	{
-		gameObject.SetActive (false);
-		//animation
-		//sound
-		//invoke game over after animation time
-		return;
+	public void TakeDamage(int dmg){
+		health -= dmg;
+		bs.TakeDamage ();
+	}
+	public void Attack(){
+		transform.position += transform.forward.normalized * speed * 0.02f;
+		pAnim.SetTrigger ("attack");
+	}
+
+	public void Die(){
+		
+	//	SceneManager.LoadScene ((SceneManager.GetActiveScene ().buildIndex + 1)%SceneManager.sceneCount); //to next level/
+		//SceneManager.LoadScene(0);	//main menu
+
+	//	Debug.Log(SceneManager.GetSceneAt(0).buildIndex); //log scene index
+		//CancelInvoke ();
+
+		theDeathScreen.gameObject.SetActive (true);
+	}
+	public void Reset(){
+		theDeathScreen.gameObject.SetActive (false);
+		//	SceneManager.LoadScene ((SceneManager.GetActiveScene ().buildIndex + 1)%SceneManager.sceneCount); //to next level/
+		SceneManager.LoadScene(1);	//main menu
+
+		//	Debug.Log(SceneManager.GetSceneAt(0).buildIndex); //log scene index
+		CancelInvoke ();
 	}
 }
